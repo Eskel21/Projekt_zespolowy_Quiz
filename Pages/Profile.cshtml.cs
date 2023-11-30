01d80f6bcb19892331bdd1dabfea03d17dde821c
Michal <michalpragacz2@gmail.com>
2023-11-30 12:34:33 +0100

    Aktualizacja mojego profilu

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,32 @@ namespace Projekt_Quizy.Pages
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+   
+
+        [BindProperty]
+        public IFormFile Picture { get; set; }
+
+        public ProfileModel(UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
+           
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (Picture != null && Picture.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await Picture.CopyToAsync(memoryStream);
+                    var user = await _userManager.GetUserAsync(User);
+                    user.Picture = memoryStream.ToArray();
+                    await _userManager.UpdateAsync(user);
+                }
+            }
+
+            return RedirectToPage("/Profile");
         }
     }
 }
