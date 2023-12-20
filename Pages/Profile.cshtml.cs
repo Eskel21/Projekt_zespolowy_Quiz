8914ad099322007333f5c6ce9bf44a01ed7307c0
Michal <michalpragacz2@gmail.com>
2023-12-20 17:15:29 +0100

    Diagram procentowy, oraz pozycja w rankingu w Mój Profil

--- Pages/Profile.cshtml.cs
+++ Pages/Profile.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Projekt_Quizy.Data;
 
 namespace Projekt_Quizy.Pages
 {
@@ -11,21 +13,37 @@ namespace Projekt_Quizy.Pages
 
         [BindProperty]
         public IFormFile Picture { get; set; }
-
-        public ProfileModel(UserManager<ApplicationUser> userManager)
+        public ApplicationUser user { get; set; }
+        private readonly ApplicationDbContext _dbContext;
+        public List<ApplicationUser> UsersRank { get; set; }
+        public int CurrentUserRank { get; set; }
+        public ProfileModel(UserManager<ApplicationUser> userManager, ApplicationDbContext dbContext)
         {
             _userManager = userManager;
+            _dbContext = dbContext;
            
         }
 
+        public async Task<IActionResult> OnGet()
+        {
+            
+            user = await _userManager.GetUserAsync(User);
+            UsersRank = _dbContext.Users.OrderByDescending(u => u.Points).ToList();
+            CurrentUserRank = UsersRank.FindIndex(u => u.Id == user.Id) + 1;
+            return Page();
+        }
+
+
+
         public async Task<IActionResult> OnPostAsync()
         {
+            
             if (Picture != null && Picture.Length > 0)
             {
                 using (var memoryStream = new MemoryStream())
                 {
                     await Picture.CopyToAsync(memoryStream);
-                    var user = await _userManager.GetUserAsync(User);
+                   
                     user.Picture = memoryStream.ToArray();
                     await _userManager.UpdateAsync(user);
                 }
